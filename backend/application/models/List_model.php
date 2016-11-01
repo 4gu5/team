90f6013b1973bf0c9e7a5ecd63d2215e63445543
mas agus <gus.dok94@gmail.com>
2016-11-01 12:20:13 +0700

    fix  json login

---
+++ backend/application/models/List_model.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class List_model extends CI_Model {
+
+	public function __construct(){
+		$this->load->database();
+	}
+
+	public function get_list(){
+		
+	}
+
+	public function input_list(){
+		
+	}
+
+	public function edit_list(){
+		
+	}
+	
+	public function delete_list(){
+		
+	}
+}
