6608107e2b4d1529325b29d3b1bbc002b627b259
mas agus <gus.dok94@gmail.com>
2016-11-01 13:39:24 +0700

    fix proses input

--- backend/application/config/routes.php
+++ backend/application/config/routes.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['login'] = 'login';
 $route['jsondata'] = 'data/get_list';
+$route['input'] = 'data/input_list';
 
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
